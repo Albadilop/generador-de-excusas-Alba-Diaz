3daba7d893b71cb84b4637200d7a698d39bf4a89
Albadilop <alba.dilop@gmail.com>
2025-02-12 12:13:17 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-import "bootstrap";
+
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let ramdomNumber = Math.random ()*10;
+  document.querySelector ('#excusa').innerHTML = generateExcuse();
+  console.log('Holaaa');
+  
+}
+let generateExcuse = function () {
+  let who = ['Mi gato', 'Mi prima', 'Mi vecino', 'Mi hermano', 'Mi tío'];
+   //array
+  let action = ['escondió', 'rompió', 'perdió', 'estropeó', 'quemó'];//array
+  let what = ['mi ordenador', 'mis apuntes', 'mis ejercicios', 'mis anotaciones','mi portátil'];//array
+  let when = ['durante el fin de semana', 'al acabar las clases', 'cuando estaba haciendo la comida', 'después de irme a domir','al amanecer'];//array
+  
+  let whoIndex = Math.floor(Math.random () * who.length);
+  let actionIndex =Math.floor(Math.random () * action.length);
+  let whatIndex =Math.floor(Math.random () * what.length);
+  let whenIndex =Math.floor(Math.random () * when.length);
+  
+  return who[whoIndex] + ' ' + action[actionIndex] + ' ' + what[whatIndex] + ' ' + when[whenIndex];
+  
 };
+
+
+
+
