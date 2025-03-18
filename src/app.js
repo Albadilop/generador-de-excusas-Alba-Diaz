88eef795c5b28bc051e2042c897d0c2a485a4fcf
Alba Diaz <alba.dilop@gmail.com>
2025-03-18 13:21:33 +0000

    fix: code improvement

--- src/app.js
+++ src/app.js
@@ -2,26 +2,23 @@
 import "./style.css";
 
 
-window.onload = function() {
-  let ramdomNumber = Math.random ()*10;
-  document.querySelector ('#excusa').innerHTML = generateExcuse();
-  console.log('Holaaa');
-  
+window.onload = function () {
+  let ramdomNumber = Math.random() * 10;
+  document.querySelector('#excusa').innerHTML = generateExcuse();
 }
 let generateExcuse = function () {
   let who = ['Mi gato', 'Mi prima', 'Mi vecino', 'Mi hermano', 'Mi tío'];
-   //array
-  let action = ['escondió', 'rompió', 'perdió', 'estropeó', 'quemó'];//array
-  let what = ['mi ordenador', 'mis apuntes', 'mis ejercicios', 'mis anotaciones','mi portátil'];//array
-  let when = ['durante el fin de semana', 'al acabar las clases', 'cuando estaba haciendo la comida', 'después de irme a domir','al amanecer'];//array
-  
-  let whoIndex = Math.floor(Math.random () * who.length);
-  let actionIndex =Math.floor(Math.random () * action.length);
-  let whatIndex =Math.floor(Math.random () * what.length);
-  let whenIndex =Math.floor(Math.random () * when.length);
-  
+
+  let action = ['escondió', 'rompió', 'perdió', 'estropeó', 'quemó'];
+  let what = ['mi ordenador', 'mis apuntes', 'mis ejercicios', 'mis anotaciones', 'mi portátil'];
+  let when = ['durante el fin de semana', 'al acabar las clases', 'cuando estaba haciendo la comida', 'después de irme a domir', 'al amanecer'];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
   return who[whoIndex] + ' ' + action[actionIndex] + ' ' + what[whatIndex] + ' ' + when[whenIndex];
-  
 };
 
 
